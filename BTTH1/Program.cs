f5e1dc8579c7dd9b5fa17343a5dcbfd5ffc79918
thien <thienphan.23160@gmail.com>
2025-10-06 21:35:09 +0700

    Add project files.

---
+++ BTTH1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+namespace MyLibrary
+{
+    public class Bai5
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nhập ngày (dd/mm/yyyy): ");
+            string input = Console.ReadLine();
+
+            DateTime date = DateTime.ParseExact(input, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
+
+
+
+
+
+
+
